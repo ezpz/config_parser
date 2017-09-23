342333a7d50a8c0f23feeab8d192cfb1c1715a05
jesse <jesse>
2017-09-23 12:24:37 -0400

    Merging in changes from dev
    commit fcb7f7b4680d48f460958900b490e0f3c51885ec
    Author: jesse <jesse>
    Date:   Sat Sep 23 12:23:50 2017 -0400
    
        Whitespace cleanup
    
    commit 6476b07d814a7ffceff0296dbc97928062aa2263
    Author: jesse <jesse>
    Date:   Sat Sep 23 12:20:41 2017 -0400
    
        Move fucntion defs
    
    commit 34dda3296048aed14961fc49f7cbae4c73f3bf04
    Author: jesse <jesse>
    Date:   Sat Sep 23 12:18:01 2017 -0400
    
        Move utility functions inside class
    
    commit 370fc51d2cbe1569d921262df2c4c03c33378f4b
    Author: jesse <jesse>
    Date:   Sat Sep 23 12:09:56 2017 -0400
    
        Adding README

--- config_parser.hh
+++ config_parser.hh
@@ -6,93 +6,23 @@
 #include <map>
 #include <cstdarg>
 
-static std::string 
-strip (const std::string &orig) {
-    const std::string ws = " \t";
-    std::string::size_type b = 0, e = 0;
-    b = orig.find_first_not_of (ws);
-    if (std::string::npos == b) { return orig; }
-    e = orig.find_last_not_of (ws);
-    if (std::string::npos == e) {
-        return orig.substr (b);
-    }
-    return orig.substr (b, (e - b) + 1);
-}
-
-static std::string 
-parse_section (const std::string &line) {
-    std::string::size_type b = 0, e = 0;
-    b = line.find ('[');
-    if (std::string::npos == b) { return ""; }
-    e = line.rfind (']'); 
-    if (std::string::npos == e) { return ""; }
-    return line.substr (b + 1, (e - b) - 1);
-}
-
-static std::pair< std::string, std::string > 
-parse_kv (const std::string &line) {
-    std::string sline = strip (line);
-    std::string::size_type n = 0;
-    n = sline.find ('=');
-    if (std::string::npos == n) { return std::make_pair ("", ""); }
-    std::string key = sline.substr (0, n);
-    std::string value = sline.substr (n + 1);
-    return std::make_pair (strip(key), strip(value));
-}
-
 class ConfigFile {
 
     typedef typename std::map< std::string, std::string > KeyValType;
     typedef typename std::map< std::string, KeyValType > SectionType;
 
     bool verbose_;
-
     std::string section_; /* current section during parse */
-
     SectionType sections_;
 
-    void Msg (const char *fmt, ...) const {
-        if (! verbose_) { return; }
-        va_list va;
-        va_start (va, fmt);
-        vfprintf (stdout, fmt, va);
-        va_end (va);
-    }
+    std::string Strip (const std::string &orig) const;
+    std::string ParseSection (const std::string &line) const;
+    std::pair< std::string, std::string > 
+        ParseKV (const std::string &line) const;
 
-    void ParseLine (const std::string &line) {
-        std::string sline = strip(line);
-        if (sline.empty ()) { return; }
-        if ('#' == sline[0] || ';' == sline[0]) { return; }
-        if ('[' == sline[0]) { 
-            std::string sec = parse_section (sline);
-            if (! sec.empty ()) {
-                Msg ("New section: [%s] => [%s]\n", 
-                        section_.c_str (), sec.c_str ());
-                section_ = sec; 
-            } else {
-                Msg ("Failed to assign new section\n");
-            }
-            return;
-        }
-        std::pair< std::string, std::string > kv = parse_kv (sline);
-        if (kv.first.empty ()) { 
-            Msg ("Failed to assign K=V\n");
-            return; 
-        }
-        Msg ("%s::%s <- '%s'\n", section_.c_str (), 
-                kv.first.c_str (), kv.second.c_str ());
-        sections_[section_][kv.first] = kv.second;
-    }
-
-    void ParseFile (const char *fname) {
-        section_ = "";
-        std::ifstream cfg(fname);
-        std::string line;
-        while (getline (cfg, line)) {
-            Msg ("INPUT: '%s'\n", line.c_str ());
-            ParseLine (line);
-        }
-    }
+    void Msg (const char *fmt, ...) const;
+    void ParseLine (const std::string &line);
+    void ParseFile (const char *fname);
 
 public:
 
@@ -108,6 +38,81 @@ public:
     std::string Get (const std::string &section, const std::string &key);
     std::string Get (const std::string &key) { return Get("",key); }
 };
+
+std::string ConfigFile::Strip (const std::string &orig) const {
+    const std::string ws = " \t";
+    std::string::size_type b = 0, e = 0;
+    b = orig.find_first_not_of (ws);
+    if (std::string::npos == b) { return orig; }
+    e = orig.find_last_not_of (ws);
+    if (std::string::npos == e) {
+        return orig.substr (b);
+    }
+    return orig.substr (b, (e - b) + 1);
+}
+
+std::string ConfigFile::ParseSection (const std::string &line) const {
+    std::string::size_type b = 0, e = 0;
+    b = line.find ('[');
+    if (std::string::npos == b) { return ""; }
+    e = line.rfind (']'); 
+    if (std::string::npos == e) { return ""; }
+    return line.substr (b + 1, (e - b) - 1);
+}
+
+std::pair< std::string, std::string > 
+ConfigFile::ParseKV (const std::string &line) const {
+    std::string sline = Strip (line);
+    std::string::size_type n = 0;
+    n = sline.find ('=');
+    if (std::string::npos == n) { return std::make_pair ("", ""); }
+    std::string key = sline.substr (0, n);
+    std::string value = sline.substr (n + 1);
+    return std::make_pair (Strip(key), Strip(value));
+}
+
+void ConfigFile::Msg (const char *fmt, ...) const {
+    if (! verbose_) { return; }
+    va_list va;
+    va_start (va, fmt);
+    vfprintf (stdout, fmt, va);
+    va_end (va);
+}
+
+void ConfigFile::ParseLine (const std::string &line) {
+    std::string sline = Strip(line);
+    if (sline.empty ()) { return; }
+    if ('#' == sline[0] || ';' == sline[0]) { return; }
+    if ('[' == sline[0]) { 
+        std::string sec = ParseSection (sline);
+        if (! sec.empty ()) {
+            Msg ("New section: [%s] => [%s]\n", 
+                    section_.c_str (), sec.c_str ());
+            section_ = sec; 
+        } else {
+            Msg ("Failed to assign new section\n");
+        }
+        return;
+    }
+    std::pair< std::string, std::string > kv = ParseKV (sline);
+    if (kv.first.empty ()) { 
+        Msg ("Failed to assign K=V\n");
+        return; 
+    }
+    Msg ("%s::%s <- '%s'\n", section_.c_str (), 
+            kv.first.c_str (), kv.second.c_str ());
+    sections_[section_][kv.first] = kv.second;
+}
+
+void ConfigFile::ParseFile (const char *fname) {
+    section_ = "";
+    std::ifstream cfg(fname);
+    std::string line;
+    while (getline (cfg, line)) {
+        Msg ("INPUT: '%s'\n", line.c_str ());
+        ParseLine (line);
+    }
+}
 
 std::string ConfigFile::Get (const std::string &sec, const std::string &key) {
     SectionType::iterator s = sections_.find (sec);
